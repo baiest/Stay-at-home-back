185aeeda3737c4a24e78b376d829a1725182007c
Juan Ballesteros <43350432+baiest@users.noreply.github.com>
2020-11-04 19:11:44 -0500

    arreglo cors

--- api.js
+++ api.js
@@ -12,7 +12,7 @@ const { Persona, Paciente, Informe } = require('./models')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors()); //Configurar quienes tienen permiso para usar el api
+app.use(cors(); //Configurar quienes tienen permiso para usar el api
 app.set('port', PORT);
 
 const funPaciente = require('./routes/paciente.js');
